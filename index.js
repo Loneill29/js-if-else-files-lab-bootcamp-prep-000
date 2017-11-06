673bd6e017a4a29b22b3a03dc28194652a0c2d53
Linda ONeill <oneill.linda21@gmail.com>
2017-11-06 14:45:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var name = "Susan"
 var height = "70"
 if (name === "Susan") {console.log ("the name is correct")}
 else {console.log ("Expected the name to be Susan")}
+if (height === "70") {console.log("the height is correct")}
 
 // Don't worry about this
 module.exports = { name, height
