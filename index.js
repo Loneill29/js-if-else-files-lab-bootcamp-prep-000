33a59652217f78b04de046576d862da119a53783
Linda ONeill <oneill.linda21@gmail.com>
2017-11-06 14:33:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74"
 if (name === "Susan") {console.log ("the name is correct")}
 else {console.log ("Expected the name to be Susan")}
@@ -6,4 +6,3 @@ else {console.log ("Expected the name to be Susan")}
 // Don't worry about this
 module.exports = { name, height
 }
-
