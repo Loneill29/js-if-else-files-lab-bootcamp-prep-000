cebd8dc7defe1c7e9a39aff55a91ce7898908a45
Linda ONeill <oneill.linda21@gmail.com>
2017-11-06 14:46:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var name = "Susan"
-var height = "70"
+var height = 70
 if (name === "Susan") {console.log ("the name is correct")}
 else {console.log ("Expected the name to be Susan")}
 if (height === "70") {console.log("the height is correct")}
